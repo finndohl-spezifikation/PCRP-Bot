a7ec98ad3b5a7074f929541c519df666a24010c2
finndohl-spezifikation <finndohl@icloud.com>
2026-04-13 15:16:53 +0200

    Add shop_raub import to bot.py

--- bot.py
+++ bot.py
@@ -61,6 +61,7 @@ import starterpaket            # Starter Paket Embed
 import vorschlag               # Vorschlag-System
 import auto_geben              # Fahrzeug-Vergabe-System
 import atm_raub               # ATM-Raub System
+import shop_raub              # Shop-Raub System
 
 try:
     import logs                 # Erweiterte Server-Logs (Voice, Name, Timeout, Rollen)
