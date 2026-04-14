5193cb2997e0991eee32d1da315e0b9450a4e698
finndohl-spezifikation <finndohl@icloud.com>
2026-04-14 04:29:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -34,6 +34,7 @@ intents.message_content = True
 intents.members = True
 intents.moderation = True
 intents.invites = True
+intents.voice_states = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 bot_start_time = None
